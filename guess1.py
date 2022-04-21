c7e70f117e60d17d3d886585c505e170ad103981
109011084 <s103390@gmail.com>
2022-04-21 11:25:06 +0800

    advance2 version

--- guess1.py
+++ guess1.py
@@ -1,9 +1,12 @@
-#產生一個隨機隨機整數1~100
+#產生一個隨機隨機整數1~100 改成 讓使用者自己設定值
 #讓使用者重複數字去猜
 #猜對了,要印出"你猜對了"
 #猜錯了,要告訴他比答案大還是小
+
 import random
-r = random.randint(1,100)
+start = int(input('請使用者輸入開始值:'))
+end = int(input('請使用者輸入結束值:'))
+r = random.randint(start,end)
 count = 0
 while True:
     count = count + 1
